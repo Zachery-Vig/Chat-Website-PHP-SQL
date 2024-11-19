da551ca97fb1da29ca07d112d837945e8b191124
Zachery-Vig <zacheryvig@outlook.com>
2024-11-18 22:19:54 -0800

    Copied Styling to index.php file.

--- index.php
+++ index.php
@@ -6,7 +6,23 @@
     <title>Message Board</title>
 </head>
 <body>
+<style>
+        * {
+            background-color: #2e2e2e;
+            color: #c9c9c9;
+            font-family: Arial;
+        }
+        form {
+            display: inline; /* Or inline-block */
+        }
+        button {
+            margin-right: 15px;
+        }
+    </style>
     <center>
+    <b style="font-size: 64px">Message Board</b>
+    <hr></hr>
+    </br>
     <form action="server.php" method="POST">
         <button style="font-size: 24px" type="submit" name="table_select_button" value="messages1">Message Board 1</button>
         <br></br>

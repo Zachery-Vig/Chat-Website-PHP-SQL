37d691b59c46ad3b1c4e3efa1d462f7102377f17
root <zacheryvig@outlook.com>
2025-01-17 20:08:08 -0800

    Added box around messages and extended messages input width.

--- chat.php
+++ chat.php
@@ -41,7 +41,7 @@ include_once("server.php")
         <br></br>
         <input style="font-size: 24px" type="text" id="user_name" placeholder="Enter Username" name="user_name"></input>
         <br></br>
-        <input style="font-size: 24px" type="text" id="user_input" placeholder="Enter Message" name="user_input" required></input>
+        <input style="font-size: 24px; width: 700px;" type="text" id="user_input" placeholder="Enter Message" name="user_input" required></input>
         <br></br>
         <button style="font-size: 24px" type="submit" id="submit_button">Submit</button>
     </form>
@@ -51,17 +51,26 @@ include_once("server.php")
     <br></br>
     <b style="font-size: 48px">Messages:</b>
     </center>
+    <br>
+    <div style="text-align: center;">
+    <div style="display: inline-block; text-align: left;">
+    <div style="border: 5px solid black; width: 800px; padding: 30px; background-color: rgb(165, 165, 165);">
+    <br>
     <?php
     if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
-            echo "<p style='font-size: 24px; position: relative; left: 20%;'>" . $row['timestamp'] . ": " . $row['name'] . ": " . $row['message'] . "</p>";
+            echo "<p style='font-size: 24px; background-color: rgb(165, 165, 165); color: rgb(0,0,0);'>" . $row['timestamp'] . ": " . $row['name'] . ": " . $row['message'] . "</p>";
         }
     } else {
-        echo "<p style='font-size: 24px; position: relative; left: 20%;'>No Messages Yet...</p>";
+        echo "<p style='font-size: 24px; color: rgb(0,0,0);'>No Messages Yet...</p>";
     }
 
     $conn->close();
     ?>
+    </div>
+    </div>
+    </div>
+    </hr>
     <script>
         //Auto saves and loads last name used to local storage.
         window.onload = function() {
@@ -71,8 +80,7 @@ include_once("server.php")
             }
         }
         document.getElementById('submit_button').addEventListener('click', function() {
-            const name = document.getElementById('user_name').value;
-            localStorage.setItem('user_name', name);
+            localStorage.setItem('user_name', document.getElementById('user_name').value);
         });
     </script>
 </body>
